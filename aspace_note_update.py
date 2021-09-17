b66dfe2941dd4d3b07e4eab6faa1153805f5fa7f
searcy <rachel.searcy@nyu.edu>
2021-09-17 12:01:28 -0400

    Update aspace_note_update.py

--- aspace_note_update.py
+++ aspace_note_update.py
@@ -1,12 +1,8 @@
 """aspace_note_update.py"""
-#This script modifies or deletes a specific note type in
-#ArchivesSpace based on information supplied by the user.
-#The user can select whether their input must simply be contained in
-#the original note text, or if it must exactly match the original note content.
+#This script modifies or deletes a specific note type in ArchivesSpace based on information supplied by the user.
+#The user can select whether their input must simply be contained in the original note text, or if it must exactly match the original note content.
 #String matches to user input are case sensitive.
-#This script is adapted from the advancedNoteEdit.py script created
-#by the Rockefeller Archives Center.
-#We borrow their original code with much gratitude.
+#This script is adapted from the advancedNoteEdit.py script created by the Rockefeller Archives Center. We borrow their original code with much gratitude.
 
 #!/usr/bin/env python
 
